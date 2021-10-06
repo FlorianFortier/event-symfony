00335d4bdedfa7223ee6f361e2a567e298172328
Florian <florian.f.imp@gmail.com>
2021-10-06 11:18:42 +0200

    dernier commit avant authsystem

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +19,84 @@ class EventController extends AbstractController
      */
     public function index(EventRepository $eventRepository): Response
     {
-        $events = $eventRepository->findAll();
+        $events = $eventRepository->findBy([], ['startedAt' => 'ASC']);
+
 
         return $this->render('event/index.html.twig', [
             'events' => $events,
         ]);
+    }
+
+    /**
+     * @Route("/event/add", name="event_add")
+     */
+    public function add(Request $request, EntityManagerInterface $em)
+    {
+        $event = new Event();
+
+        $addEventForm = $this->createForm(EventType::class, $event);
+
+        $addEventForm->handleRequest($request);
+
+        if ($addEventForm->isSubmitted() && $addEventForm->isValid()) {
+            $event = $addEventForm->getData();
+
+            $em->persist($event);
+            $em->flush();
+
+            return $this->redirectToRoute('event_show', [
+                'event' => $event->getId()
+            ]);
+        }
+
+        return $this->render('event/add.html.twig', [
+            'addEventForm' => $addEventForm->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/event/{event}/update", name="event_update")
+     */
+    public function update(Event $event, Request $request, EntityManagerInterface $em)
+    {
+        $updateEventForm = $this->createForm(EventType::class, $event);
+
+        $updateEventForm->handleRequest($request);
+
+        if ($updateEventForm->isSubmitted() && $updateEventForm->isValid()) {
+            $em->flush();
+        }
+
+        return $this->render('event/update.html.twig', [
+            'updateEventForm' => $updateEventForm->createView(),
+            'eventName' => $event->getName()
+        ]);
+
+    }
+
+    /**
+     * @Route("/event/{event}/delete", name="event_delete")
+     */
+    public function delete(Event $event, EntityManagerInterface $em)
+    {
+        $delete = $event->getName().'a bien été supprimé !';
+        $em->remove($event);
+        $em->flush();
+
+        $this->addFlash('success', $delete);
+        return $this->redirectToRoute('event');
+
+    }
+
+
+    /**
+     * @Route("/event/{event}", name="event_show")
+     */
+    public function  show(Event $event)
+    {
+        return $this->render('event/show.html.twig', [
+            'event' => $event
+    ]);
     }
 }
---
+++ src/Form/EventType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('startedAt')
+            ->add('endedAt')
+            ->add('placeNumber')
+            ->add('description')
+            ->add('image')
+            ->add('envoyez', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'event' => Event::class,
+        ]);
+    }
+}
